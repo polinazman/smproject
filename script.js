23b1738362e7090ba3fcb82f851129c04f60e5ab
Polina <polina.j4@gmail.com>
2019-12-03 23:28:56 +0500

    Modify character cards

--- script.js
+++ script.js
@@ -11,8 +11,40 @@ fetch("https://www.anapioficeandfire.com/api/books/2")
 			.then((response) => {
 				return response.json();
 			})
-			.then(jsonResult => {
-				console.log(jsonResult.name);
+			.then(cardResult=> {
+				console.log(cardResult.name);
+				var cards = document.getElementById("cards");
+
+				var outerDiv = document.createElement("div");
+				outerDiv.setAttribute("class", "col-sm-3half");
+				cards.appendChild(outerDiv);
+
+				var innerDiv = document.createElement("div");
+				innerDiv.setAttribute("class", "card-container");
+				outerDiv.appendChild(innerDiv);
+
+				var h4 = document.createElement("h4");
+				h4.innerHTML = cardResult.name;
+				innerDiv.appendChild(h4);
+
+				var gender = document.createElement("p");
+				gender.innerHTML = "Gender: " + cardResult.gender;
+				gender.setAttribute("class", "gender");
+				innerDiv.appendChild(gender);
+
+				if(cardResult.culture !== "") {
+					var culture = document.createElement("p");
+					culture.innerHTML = "Culture: " + cardResult.culture;
+					culture.setAttribute("class", "culture");
+					innerDiv.appendChild(culture);
+				}
+
+				if(cardResult.titles !== "") {
+					var titles = document.createElement("p");
+					titles.innerHTML = "Titles: " + cardResult.titles;
+					titles.setAttribute("class", "titles");
+					innerDiv.appendChild(titles);
+				}
 			})
 		}
 	})
