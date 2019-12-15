878c6b3e9367040d87f1f3a38e53163a2b267bbe
Polina <polina.j4@gmail.com>
2019-12-15 10:40:17 +0500

    Game features fix

--- script.js
+++ script.js
@@ -91,115 +91,154 @@ fetch("https://www.anapioficeandfire.com/api/books/2")
 						$(".game-container").toggleClass("pregame");
 						$(".subhead").addClass("game");
 						$("#characters").addClass("game");
-						
+
+						var playerName1 = document.getElementById("player1");
+						playerName1.innerHTML = player1;
+						var playerName2 = document.getElementById("player2");
+						playerName2.innerHTML = player2;
+
 						boardGame();
-					}
-
+					}						
 				};
-				//Game function 
-				
-				function boardGame() {
-		
-					var playerName1 = document.getElementById("player1");
-					playerName1.innerHTML = player1;
-					var playerName2 = document.getElementById("player2");
-					playerName2.innerHTML = player2;
-
-					var rollButton = document.getElementById("dice-roll");
-					rollButton.onclick = Game.takeTurn;
-
-					Game.populateBoard();
-				};
-
-				var Game = (function() {
-					var game = {};
-
-					game.tiles = 30;
-
-					game.players = [
-					new Player(playerName1, "Triangle", "player1"),
-					new Player(playerName2, "Circle", "player2")
-					];
-
-					game.currentPlayer = 0;
-
-					game.populateBoard = function() {
-					var id = this.tiles[i].squareID;
-					var tileNumber = document.getElementById(id + "-number");
-					tileNumber.innerHTML = this.squares[i].name;
-
-					var startTile = document.getElementById("startTile-residents");
-					for (var i = 0; i < game.players.length; i++){
-						game.players[i].createToken(tile1);
-					}
-
-					game.takeTurn = function() {
-						movePlayer();
-
-						game.currentPlayer = nextPlayer(game.currentPlayer);
-
-						updateByID("currentTurn", game.players[game.currentPlayer]);
-					};
-
-					function nextPlayer(currentPlayer) {
-						var nextPlayer = currentPlayer + 1;
-
-						if (nextPlayer === game.players.length){
-							return 0;
-						}
-
-						return nextPlayer;
-					}
-
-					function movePlayer() {
-						var moves = Math.floor(Math.random() * 6) + 1;
-						var totalTiles = game.tiles + 1;
-						var currentPlayer = game.players[game.currentPlayer];
-						var currentTile = parseInt(currentPlayer.currentTile.slice(6));
-
-						currentPlayer.currentTile = "tile" + nextTile;
-
-						var currentToken = document.getElementById(currentPlayer.id);
-						currentToken.parentNode.removeChild(currentToken);
-
-						currentPlayer.createToken(document.getElementById(currentPlayer.currentTile));
-					}
-
-					function checkTile() {
-						var currentTileId = currentPlayer.currentTile;
-
-						if (currentTileId === "tile30")
-							currentPlayer //has won the game
-					}
-
-					function updateById(id, msg) {
-						document.getElementById(id).innerHTML = msg;
-					}
-
-					function Tile(number) {
-						this.number = number;
-					}
-					
-					function Player(name, token, id) {
-					    this.name = name;
-					    this.token = token;
-					    this.id = id;
-					    this.currentTile = "tile1";
-					}
-
-					Player.prototype.createToken = function(tile) {
-						var playerSpan = document.createElement("span");
-						playerSpan.setAttribute("class", this.token);
-						playerSpan.setAttribute("id", this.id);
-						tile.appendChild(playerSpan);
-					};
-				};
-			}
-
-		);
-				
 			});
-			
 		}
 	});
 });
+				
+function boardGame() {
+
+	var rollButton = document.getElementById("dice-roll");
+	rollButton.onclick = Game.takeTurn;
+
+	Game.populateBoard();
+};
+
+var Game = (function() {
+	var game = {};
+
+	game.tiles = [
+		new Tile("tile1"),
+		new Tile("tile2"),
+		new Tile("tile3"),
+		new Tile("tile4"),
+		new Tile("tile5"),
+		new Tile("tile6"),
+		new Tile("tile7"),
+		new Tile("tile8"),
+		new Tile("tile9"),
+		new Tile("tile10"),
+		new Tile("tile11"),
+		new Tile("tile12"),
+		new Tile("tile13"),
+		new Tile("tile14"),
+		new Tile("tile15"),
+		new Tile("tile16"),
+		new Tile("tile17"),
+		new Tile("tile18"),
+		new Tile("tile19"),
+		new Tile("tile20"),
+		new Tile("tile21"),
+		new Tile("tile22"),
+		new Tile("tile23"),
+		new Tile("tile24"),
+		new Tile("tile25"),
+		new Tile("tile26"),
+		new Tile("tile27"),
+		new Tile("tile28"),
+		new Tile("tile29"),
+		new Tile("tile30")
+	];
+
+	game.players = [
+		new Player(document.getElementById("player1"), "Triangle", "player1"),
+		new Player(document.getElementById("player2"), "Circle", "player2")
+	];
+
+	game.currentPlayer = 0;
+
+	game.populateBoard = function() {
+		for (var i = 0; i < this.tiles.length; i++) {
+			var id = this.tiles[i].tileID;
+		}
+
+		var tile1 = document.getElementById("tile1-residents");
+		for (var i = 0; i < game.players.length; i++){
+			game.players[i].createToken(tile1);
+		}
+	};
+
+	game.takeTurn = function() {
+		movePlayer();
+
+		checkTile();
+	};
+
+	function nextPlayer(currentPlayer) {
+		var nextPlayer = currentPlayer + 1;
+
+		if (nextPlayer === game.players.length){
+			return 0;
+		}
+
+		return nextPlayer;
+	}
+
+	function movePlayer() {
+		var rolled = Math.floor(Math.random() * 6) + 1;
+		var url = "graphics/dice" + rolled + ".png";
+		var diceResult = document.getElementById("dice");
+		diceResult.setAttribute("src", url);
+		var moves = rolled;
+		var totalTiles = game.tiles.length + 1;
+		var currentPlayer = game.players[game.currentPlayer];
+		var currentTile = parseInt(currentPlayer.currentTile.slice(4));
+		var nextTile = currentTile + moves;
+
+		if (moves === 6) {
+			game.currentPlayer = game.currentPlayer;
+			alert("You get another turn");
+			
+		} else {
+			game.currentPlayer = nextPlayer(game.currentPlayer);
+		}
+
+		if (currentTile + moves <= totalTiles) {
+			var nextTile = currentTile + moves;
+		} else {
+			alert("Congratulations " + game.players[game.currentPlayer].name.innerHTML + ", you win!");
+		}
+
+		currentPlayer.currentTile = "tile" + nextTile;
+
+		var currentToken = document.getElementById(currentPlayer.id + "-token");
+		console.log(currentToken);
+		currentToken.parentNode.removeChild(currentToken);
+
+		currentPlayer.createToken(document.getElementById(currentPlayer.currentTile));
+	}
+
+	function checkTile() {
+		var currentPlayer = game.players[game.currentPlayer];
+		var currentTileId = currentPlayer.currentTile;
+		console.log(currentTileId);
+	}
+
+	function Tile(tileID) {
+		this.tileID = tileID;
+	}
+	
+	function Player(name, token, id) {
+	    this.name = name;
+	    this.token = token;
+	    this.id = id;
+	    this.currentTile = "tile1";
+	}
+
+	Player.prototype.createToken = function(tile) {
+		var playerSpan = document.createElement("span");
+		playerSpan.setAttribute("class", this.token);
+		playerSpan.setAttribute("id", this.id + "-token");
+		tile.appendChild(playerSpan);
+	};
+	return game;
+})();
